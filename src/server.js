1b9ef68a774ee49401c74c3c7fa48ae5e1219892
21127615 <bghuy2003@gmail.com>
2023-11-17 16:42:07 +0700

    add stactic

--- src/server.js
+++ src/server.js
@@ -7,11 +7,15 @@ require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
-
+//config view engine
 app.set('views', path.join(__dirname, 'views'));// use dirname to let nodejs understand the direction
-
 app.set('view engine', 'ejs');
 
+
+// config static file
+app.use(express.static(path.join(__dirname, 'public')))
+
+//declare rout
 app.get('/', (req, res) => {
     res.render('./sample.ejs')
 })
