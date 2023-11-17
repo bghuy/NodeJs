a60d22e52c2215a68271ff00218083da5bfc3702
21127615 <bghuy2003@gmail.com>
2023-11-17 09:39:27 +0700

    test env

--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
 
 const express = require('express')
 const path = require('path');//require path to get to folder
+require('dotenv').config();
+
+
 const app = express()
-const port = 8081
-
 app.set('views', path.join(__dirname, 'views'));// use dirname to let nodejs understand the direction
-
 app.set('view engine', 'ejs');
+
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
 
 app.get('/', (req, res) => {
     res.render('./sample.ejs')
 })
-
-app.listen(port, () => {
+app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
 })
